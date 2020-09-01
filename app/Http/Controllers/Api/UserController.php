a05860f22790a18458c8c9e149da2ae2a438ee95
Rizal Muhamad H <rizal.muhamad@ayomedia.com>
2020-09-01 09:31:43 +0700

    add package image

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Intervention\Image\Facades\Image;
 
 class UserController extends Controller
 {
+
+    public $path;
+    public $dimensions;
+
+    public function __construct()
+    {
+        $this->path = storage_path('app/public/users');
+        $this->dimensions = ['245', '300', '500'];
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -43,7 +56,12 @@ class UserController extends Controller
 
     public function profile(User $user)
     {
-        return response($user, 201);
+        $response = [
+            'code' => 200,
+            'data' => new UserResource($user)
+        ];
+
+        return response($response, 200);
     }
 
     /**
@@ -77,7 +95,83 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        if ($user) {
+        }
+    }
+
+    public function updateName(Request $request, $id)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if ($user) {
+            $user->update(["name" => $request->name]);
+
+            $response = [
+                'code' => 200,
+                'data' => new UserResource($user)
+            ];
+        } else {
+            $response = [
+                'code' => 500,
+                'data' => null
+            ];
+        }
+
+        return response($response, $response->code);
+    }
+
+    public function updatePassword(Request $request, $id)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if ($user) {
+            $user->update(["password" => bcrypt($request->password)]);
+
+            $response = [
+                'code' => 200,
+                'data' => new UserResource($user)
+            ];
+        } else {
+            $response = [
+                'code' => 500,
+                'data' => null
+            ];
+        }
+
+        return response($response, $response->code);
+    }
+
+    public function updateAvatar(Request $request, $id)
+    {
+        $this->validate($request, [
+            'avatar' => 'required|image|mimes:jpg,png,jpeg'
+        ]);
+
+
+        $user = User::where('email', $request->email)->first();
+
+        if ($user) {
+            $file = $request->file("avatar");
+            $fileName = Carbon::now()->timestamp . '_' . uniqid() . '.' . $file->getClientOriginalExtension();
+
+            Image::make($file)->save($this->path . '/' . $fileName);
+
+            $user->update(["avatar" => "users/" . $fileName]);
+
+            $response = [
+                'code' => 200,
+                'data' => new UserResource($user)
+            ];
+        } else {
+            $response = [
+                'code' => 500,
+                'data' => null
+            ];
+        }
+
+        return response($response, $response->code);
     }
 
     /**
@@ -91,3 +185,5 @@ class UserController extends Controller
         //
     }
 }
+
+// https://daengweb.id/upload-resize-image-di-laravel-57
