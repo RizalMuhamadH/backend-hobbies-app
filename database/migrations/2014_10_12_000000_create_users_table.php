ca552bd10f7d119ad40b2897779fdf9dbef7b595
Rizal Muhamad H <rizal.muhamad@ayomedia.com>
2020-09-08 14:44:55 +0700

    notify nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('notify_token');
+            $table->string('notify_token')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
