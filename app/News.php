4dfe2b1ecc1005ad5c3d0bdbe566b26a84207097
Rizal Muhamad H <rizal.muhamad@ayomedia.com>
2020-09-16 16:46:49 +0700

    save admin

--- app/News.php
+++ app/News.php
@@ -3,9 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use TCG\Voyager\Traits\Translatable;
 
 class News extends Model
 {
+
+    use Translatable;
+
+    public function save(array $options = [])
+    {
+        // If no author has been assigned, assign the current user's id as the author of the post
+        if (!$this->admin_id && Auth::user()) {
+            $this->admin_id = Auth::user()->getKey();
+        }
+
+        return parent::save();
+    }
+
     public function image()
     {
         return $this->morphOne(Image::class, 'imageable');
