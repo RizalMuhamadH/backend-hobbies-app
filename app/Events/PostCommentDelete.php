3d94e5561983a10fc1b13afe3edc5b0f8fca34c7
Rizal Muhamad H <rizal.muhamad@ayomedia.com>
2020-12-16 16:55:26 +0700

    events comment

---
+++ app/Events/PostCommentDelete.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use App\Comment;
+use App\Http\Resources\CommentResource;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PostCommentDelete
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+
+    public $comment;
+    public function __construct(Comment $comment)
+    {
+        $this->comment = new CommentResource($comment);
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('post-' . $this->comment->commentable_id . '-comment');
+    }
+}
---
+++ app/Events/PostCommentSent.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use App\Comment;
+use App\Http\Resources\CommentResource;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PostCommentSent implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+
+    public $comment;
+
+    public function __construct(Comment $comment)
+    {
+        $this->comment = new CommentResource($comment);
+        // $this->comment = $comment;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('post-' . $this->comment->commentable_id . '-comment');
+    }
+}
