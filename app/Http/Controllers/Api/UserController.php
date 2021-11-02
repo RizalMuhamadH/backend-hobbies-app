b959d15ce9159fc0b2d3a537e00f51ca9e1adb03
RizalMuhamadH <rizalmuhamadh@gmail.com>
2021-11-02 11:09:18 +0100

    update hobbies

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -163,10 +163,10 @@ class UserController extends Controller
             if ($request->password != null)
                 $user->update(["password" => bcrypt($request->password)]);
 
-            if ($request->hobby != $user->hobbies()->get()[0]->id) {
+            // if ($request->hobby != $user->hobbies()->get()[0]->id) {
                 $user->hobbies()->detach();
                 $user->hobbies()->attach($request->hobbies);
-            }
+            // }
 
             if ($user)
                 return (new UserProfileResource($user))->additional(['meta' => [
